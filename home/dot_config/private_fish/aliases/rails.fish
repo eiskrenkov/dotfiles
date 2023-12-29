16f50674cf9bee8ec784040fa3df17ac5e9d9ec8
Egor Iskrenkov <egor@iskrenkov.me>
2023-12-29 11:33:47 +0100

    Prefix rails and rake with bundle exec

--- home/dot_config/private_fish/aliases/rails.fish
+++ home/dot_config/private_fish/aliases/rails.fish
@@ -1,15 +1,18 @@
 # Rails aliases
-alias rs='rails server'
-alias fs="foreman s"
-alias rc='rails console'
+alias rs='bundle exec rails server'
+alias rc='bundle exec rails console'
 
-alias rgm='rails generate migration'
-alias rdm='rake db:migrate'
-alias rdmt="rake db:migrate RAILS_ENV=test"
+alias rgm='bundle exec rails generate migration'
+alias rdm='bundle exec rake db:migrate'
+alias rdmt="bundle exec rake db:migrate RAILS_ENV=test"
 
 alias devlog='tail -f log/development.log'
 alias testlog='tail -f log/test.log'
 
-alias rr='rails routes'
+alias rr='bundle exec rails routes'
+alias rt='bundle exec rake -T'
 
-alias rup='ggpull && bundle install && SKIP_ANNOTATE=true rake db:migrate'
+alias rup='ggpull && bundle install && SKIP_ANNOTATE=true bundle exec rake db:migrate'
+
+alias bi='bundle install'
+alias be='bundle exec'
