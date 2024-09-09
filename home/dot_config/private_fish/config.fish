65423ada0bc33cb85087c8fee590199e11c6396b
Egor Iskrenkov <egor@iskrenkov.me>
2024-09-09 16:11:47 +0200

    Update alacritty

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -32,8 +32,6 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 
-set -gx TMUX_WINDOWIZER_COMMAND "find ~/tds ~/dev -mindepth 1 -maxdepth 1 -type d; echo ~/.local/share/chezmoi"
-
 # Mise
 mise activate fish | source
 
