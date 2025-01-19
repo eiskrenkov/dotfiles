0b54babe8ae8ee1dd098853c5236873bfbbd0f1d
Egor Iskrenkov <egor@iskrenkov.me>
2025-01-19 19:03:58 +0100

    Fix default branch matcher

--- home/dot_config/private_fish/functions/main_branch.fish
+++ home/dot_config/private_fish/functions/main_branch.fish
@@ -1,3 +1,3 @@
 function main_branch
-  git branch -r | grep -E -i '^\s.origin\/(master|main)' | cut -d/ -f2 | head -n 1
+  git branch -r | grep -E -i '^\s.origin\/(master|main)$' | cut -d/ -f2 | head -n 1
 end
