2cd406c9dc33cd10fb94f7a76e280c5cf6bc26f9
Egor Iskrenkov <egor@iskrenkov.me>
2025-04-01 14:46:05 +0200

    Orbstack init command

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -42,3 +42,7 @@ fzf --fish | source
 
 # zoxide
 zoxide init --cmd cd fish | source
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
