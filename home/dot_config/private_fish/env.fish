efbe4e4a0334a20e4668db5b45ac249d6ebf84de
Egor Iskrenkov <egor@iskrenkov.me>
2026-01-20 17:37:54 +0100

    Drop global gitignore

--- home/dot_config/private_fish/env.fish
+++ home/dot_config/private_fish/env.fish
@@ -12,8 +12,6 @@ set -gx LDFLAGS '-L/opt/homebrew/opt/openssl@3/lib'
 set -gx CPPFLAGS '-I/opt/homebrew/opt/openssl@3/include'
 set -gx PKG_CONFIG_PATH /opt/homebrew/opt/openssl@3/lib/pkgconfig
 
-set -gx OPENAI_API_KEY "op://Personal/j2mxdpu7m536rh7rl4dujrj3cq/credential"
-
 # Set fzf options
 # set -gx FZF_DEFAULT_OPTS '--height 30% --layout=reverse --border'
 
